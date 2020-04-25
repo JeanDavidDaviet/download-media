352d5aa413434c377f21793189165095d7af79b8
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2020-04-25 12:19:13 +0200

    initial commit

---
+++ download-media.php
@@ -0,0 +1,159 @@
+<?php
+/*
+Plugin Name: Download Media
+Plugin URI: http://wordpress.org/plugins/download-media/
+Description: Allows images in the media library to be direclty download one by one or in bulk.
+Author: Jean-David Daviet
+Version: 1.0
+Author URI: https://jeandaviddaviet.fr
+Text Domain: download-image
+*/
+
+add_filter( 'media_row_actions', 'dm_add_download_link_to_media_list_view', 10, 2 );
+add_filter( 'attachment_fields_to_edit', 'dm_add_download_link_to_edit_media_modal_fields_area', 10, 2 );
+
+// For the bulk action dropdowns.
+add_action( 'admin_head-upload.php', 'dm_add_bulk_actions_via_javascript' );
+add_action( 'admin_action_bulk_download_image', 'dm_bulk_action_handler' ); // Top drowndown.
+add_action( 'admin_action_-1', 'dm_bulk_action_handler' ); // Bottom dropdown.
+add_action( 'admin_notices', 'dm_admin_notice_error' );
+
+
+function dm_add_download_link_to_media_list_view($actions, $post){  
+  if( ! current_user_can( 'upload_files' ) ) {
+    return $actions;
+  }
+
+  $actions['download_image'] = dm_generate_link_for_image($post);  
+  return $actions;
+}
+
+
+function dm_add_download_link_to_edit_media_modal_fields_area( $form_fields, $post ) {
+
+  if( ! current_user_can( 'upload_files' ) ) {
+    return $form_fields;
+  }
+  
+  $form_fields['download_image'] = array(
+    'label'         => '',
+    'input'         => 'html',
+    'html'          => dm_generate_link_for_image($post, 'button-secondary button-large'),
+    'show_in_modal' => true,
+    'show_in_edit'  => false,
+  );
+  return $form_fields;
+}
+
+
+function dm_generate_link_for_image($post, $class = ''){
+  $title = apply_filters('the_title', $post->post_title);
+  return '<a download="' . esc_attr( $title ) . '" href="' . esc_attr( wp_get_attachment_image_url( $post->ID, 'full' ) ) . '" title="' . esc_attr( __( 'Download this image', 'download-image' ) ) . '" class="' . $class . '">' . _x( 'Download this image', 'action for a single image', 'download-image' ) . '</a>';
+}
+
+
+function dm_add_bulk_actions_via_javascript() {
+  if ( ! current_user_can( 'upload_files' ) || ! class_exists('ZipArchive') ) {
+    return;
+  }
+
+  ?>
+  <script type="text/javascript">
+    jQuery(document).ready(function ($) {
+      $('select[name^="action"] option:last-child').before(
+        $('<option/>')
+          .attr('value', 'bulk_download_image')
+          .text('<?php echo esc_js( _x( 'Download the selected images', 'bulk actions dropdown', 'download-image' ) ); ?>')
+      );
+    });
+  </script>
+  <?php
+}
+
+function dm_bulk_action_handler() {  
+  if (empty( $_REQUEST['action'] ) || empty( $_REQUEST['action2'] ) || ( 'bulk_download_image' != $_REQUEST['action'] && 'bulk_download_image' != $_REQUEST['action2'] ) || empty( $_REQUEST['media'] ) || ! is_array( $_REQUEST['media'] )) {
+    return;
+  }
+
+  check_admin_referer( 'bulk-media' );
+
+  $errors = new WP_Error();
+
+  if( class_exists('ZipArchive') ){
+    $errors->add( 'zip_archive_class', __('The ZipArchive PHP Library isn\'t installed.' , 'download-media') );
+    dm_displayError( $errors );
+  }
+  
+  $zip = new ZipArchive();
+  $zip_path = __DIR__ . DIRECTORY_SEPARATOR . "dm_" . time() . ".zip";
+  
+  if ( $zip->open( $zip_path, ZipArchive::CREATE ) !== true ) {
+    /* translators: %s: Generated name of the zipfile */
+    $errors->add( 'open_zip', sprintf( __('Can\'t open the zip file %' , 'download-media'), $zip_path ) );
+    dm_displayError( $errors );
+  }
+  
+  foreach($_REQUEST['media'] as $media){
+    $title = get_the_title($media);
+    $media_path = get_attached_file((int) $media);
+    $extension = pathinfo($media_path);
+    $extension = $extension['extension'];
+    $filename = $title . '.' . $extension;
+
+    if(file_exists($media_path)) {
+      if(is_readable($media_path)) {
+        $zip->addFile($media_path, $filename);
+      }else{
+        $errors->add( 'is_not_readable', sprintf( __('The file %s isn\'t readable' , 'download-media'), $filename ) );
+      }
+      $errors->add( 'file_doesnt_exist', sprintf( __('The file %s doesn\'t exist.' , 'download-media'), $filename ) );
+    }
+  }
+
+  if ( $errors->has_errors() ) {
+    dm_displayError( $errors );
+  }
+
+  if( $zip->close() !== true){
+    $errors->add( 'cant_create_zip', __('Something wrong appened when trying to create the ZIP file.' , 'download-media') );
+    dm_displayError( $errors );
+  }
+
+  if(file_exists($zip_path)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="' . basename($zip_path) . '"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($zip_path));
+    flush(); // Flush system output buffer
+    readfile($zip_path);
+    exit();
+  }else{
+    $errors->add( 'cant_download_zip', __('Something wrong appened when trying to download the ZIP file.' , 'download-media') );
+    dm_displayError( $errors );
+  }
+
+  exit();
+}
+
+function dm_displayError( $errors ){
+  set_transient( 'download_media_error_notice', $errors );
+  exit();
+}
+
+function dm_admin_notice_error() {
+  $error = get_transient('download_media_error_notice');
+  if(is_wp_error($error)):
+    $error_messages = $error->get_error_messages();
+    delete_transient('download_media_error_notice');
+
+    foreach($error_messages as $error_message): ?>
+    <div class="notice notice-error is-dismissible">
+      <p><?php echo $error_message; ?></p>
+    </div>
+    <?php
+    endforeach;
+  endif;
+}
---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== Download Media ===
+Contributors: jean-david
+Tags: image, download, library, post thumbnails
+Requires at least: 4.7
+Tested up to: 5.5
+Requires PHP: 5.2.4
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Allows images in the media library to be direclty download one by one or in bulk.
+
+== Description ==
+
+Download Image allows you to directly download images from the media library to your device in one click.
+
+You can download images one by one, or in bulk.
+
+= Need Help? Found A Bug? Want To Contribute Code? =
+
+Support for this plugin is provided via the [WordPress.org forums](https://wordpress.org/support/plugin/download-image).
+
+The source code for this plugin is available on [GitHub](https://github.com/JeanDavidDaviet/download-image).
+
+== Installation ==
+
+1. Go to your admin area and select Plugins → Add New from the menu.
+2. Search for "Download Image".
+3. Click install.
+4. Click activate.
+5. Navigate to Media → Library
+
+== ChangeLog ==
+
+= Version 1.0.0 =
+
+* Initial release.
